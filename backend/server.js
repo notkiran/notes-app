753829a8e56205f5bf6f71e36498563cce8d9a55
Kiran G <mail@kirang.me>
2022-02-23 19:46:58 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,14 @@ app.use("/api/notes", noteRoutes);
 
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
   });
 }
 
